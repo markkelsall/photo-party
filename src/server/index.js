cdff89d6c65ac9cf912111ec25e04701667289b4
Mark Kelsall <mark.kelsall@gmail.com>
2018-08-25 21:38:44 +0100

    add winston and morgan

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,30 @@
 import express from 'express';
 import os from 'os';
+import morgan from 'morgan';
+import logger from './logger';
+
+const morganEnv = process.env.NODE_ENV === 'dev' ? 'dev' : 'common';
+const PORT = 3000;
 
 const app = express();
 
+app.use(morgan(morganEnv, {
+    skip: function (req, res) {
+        return res.statusCode < 400
+    }, stream: process.stderr
+}));
+
+app.use(morgan(morganEnv, {
+    skip: function (req, res) {
+        return res.statusCode >= 400
+    }, stream: process.stdout
+}));
+
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+app.get('/api/getUsername', (req, res) => {
+    logger.debug('Debug statement');
+    logger.info('Info statement');
+    res.send({username: os.userInfo().username})
+});
+
+app.listen(8080, () => logger.info('Listening on port: ' + PORT));
